99aa6e6388ee383755899de1a3b5238a4e13a3bb
AzizMakhmudov <azizmakhmudov2412@gmail.com>
2023-07-21 23:57:57 +0500

    fix bug in card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Scrollbar, A11y } from 'swiper/modules';
 import 'swiper/css';
@@ -10,6 +10,24 @@ import { useNavigate } from 'react-router-dom'
 
 export default function Card({ title, description, price, img1, img2, img3, id }) {
   const navigate = useNavigate()
+  const [added, setAdded] = useState(false);
+
+  function handleAddToCart() {
+    const product = { id: id, title: title, image: img1, count: 1, price: price }
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    const index = cart.findIndex((item) => item.id === product.id);
+
+    if (index === -1) {
+      cart.push({ ...product, count: 1 });
+      localStorage.setItem("cart", JSON.stringify(cart));
+      setAdded(true);
+    } else {
+      cart.splice(index, 1);
+      localStorage.setItem("cart", JSON.stringify(cart));
+      setAdded(false);
+    }
+  }
+
   return (
     <>
       <div className="container">
@@ -29,7 +47,7 @@ export default function Card({ title, description, price, img1, img2, img3, id }
             <SwiperSlide><img width={'100%'} height={'auto'} src={img2} alt="swiper" /></SwiperSlide>
             <SwiperSlide><img width={'100%'} height={'auto'} src={img3} alt="swiper" /></SwiperSlide>
           </Swiper>
-          <button className='myCard__btn--add'>Add to Cart</button>
+          <button className='myCard__btn--add' onClick={handleAddToCart}>{added ? "Added" : "Add to Cart"}</button>
           <button onClick={() => navigate("/")} className='myCard__btn--back'>Back to Home</button>
         </section>
         <section className="similar">
